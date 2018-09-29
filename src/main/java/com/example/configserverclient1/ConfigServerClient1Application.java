55146ce810ab3f310c20baaa963e1c2de2c396f5
PratikAzad <pkazad23@gmail.com>
2018-09-29 18:32:41 +0530

    first commit

---
+++ src/main/java/com/example/configserverclient1/ConfigServerClient1Application.java
@@ -0,0 +1,12 @@
+package com.example.configserverclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerClient1Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/configserverclient1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.configserverclient1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConfigServerClient1Application.class);
+	}
+
+}
